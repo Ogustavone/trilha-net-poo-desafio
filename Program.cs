0954712f68805508b038444c99be976bcf8900c6
Gustavo S. <ogustavone@gmail.com>
2025-06-17 17:26:00 -0300

    Desafio completo, implementação de propriedades e métodos em Models

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Lumia 520", imei: "1234512345", memoria: 4);
+Smartphone iphone = new Iphone(numero: "54321", modelo: "8 Plus", imei: "5432154321", memoria: 4);
+
+// Teste Nokia
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.Write("\n");
+
+// Teste Iphone
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+// Teste propriedades
+Console.WriteLine("\nEspecificações Nokia");
+Console.WriteLine($"- Número: {nokia.Numero}\n- Modelo: {nokia.Modelo}");
+Console.WriteLine($"- Imei: {nokia.IMEI}\n- Memória: {nokia.Memoria}");
+
+Console.WriteLine("\nEspecificações Iphone");
+Console.WriteLine($"- Número: {iphone.Numero}\n- Modelo: {iphone.Modelo}");
+Console.WriteLine($"- Imei: {iphone.IMEI}\n- Memória: {iphone.Memoria}");
